65d7f5a2ec1147d2cba6bd5c8d6248e76a8c8281
spancavil <soancavil@gmail.com>
2023-05-23 20:22:18 -0300

    project initialization

---
+++ src/main/java/com/coderhouse/clasegit/ClasegitApplication.java
@@ -0,0 +1,13 @@
+package com.coderhouse.clasegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasegitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasegitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderhouse/clasegit/ClasegitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderhouse.clasegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasegitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
