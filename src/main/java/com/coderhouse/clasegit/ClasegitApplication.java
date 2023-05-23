b6ebd129a7bf7476ecb7680bcf4fc213e7f09c6d
spancavil <soancavil@gmail.com>
2023-05-23 20:47:43 -0300

    Added endpoint Hola. Todo: fix endpoint

--- src/main/java/com/coderhouse/clasegit/ClasegitApplication.java
+++ src/main/java/com/coderhouse/clasegit/ClasegitApplication.java
@@ -1,13 +1,18 @@
 package com.coderhouse.clasegit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClasegitApplication {
+public class ClasegitApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClasegitApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server listening. Access H2 on: http://localhost:8888/h2-console");
+	}
 }
---
+++ src/main/java/controller/Hola.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hola")
+public class Hola {
+    @GetMapping
+    public String getHola(){
+        return "Hola coders!";
+    }
+}
