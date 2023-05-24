b87abee5f578f151ee6c514725e3ed3e0a7d994f
“nicolassistema” <nicolas.letticugna@gmail.com>
2023-05-23 21:39:12 -0300

    subiendo cambios

--- src/main/java/controller/Hola.java
+++ src/main/java/controller/Hola.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hola {
     @GetMapping
     public String getHola(){
-        return "chau coders!";
+        return "modificadp coders!";
     }
 }
